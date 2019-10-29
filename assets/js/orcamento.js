f337eb51bbfb5cfe9b65e6f0e05be807b8de9f3d
gustavogavasussi <gustavogavasussi14@outlook.com>
2019-10-29 20:17:23 -0300

    29/10

--- assets/js/orcamento.js
+++ assets/js/orcamento.js
@@ -1,27 +1,85 @@
-var arrayMateriais = [ 'Caneta','Lápis','Borracha','Marca-Texto','Tesoura','Cola','Régua','Calculadora','Apontador','Corretivo'];
+const TIPO_MATERIAL = ["Caneta", "Lapis", "Caderno", "Borracha", "Apontador", "Régua", "Corretivo", "Marca-texto", "Cola", "Tesoura"];
+const materialEscolar = document.getElementById("escolhaOpcao");
+var item = {};
 
-const materialEscolar = document.getElementById("materialEscolar");
-
-arrayMateriais.forEach((material) => {
-    option = new Option(material, material.toLowerCase());
-    materialEscolar.options[materialEscolar.options.length] = option;
-  });
-
-
-function habilitarBotao() {
-if ($('#nomeAluno').valid())
-   ($('#gastarMaterial').valid());{
-  document.getElementById("Button").disabled = false;
-}
-}
-else{
-  document.getElementById("Button").disabled=true;
-}
-
-$("#nomeAluno").focusout(function focusOutz(){
- habilitarBotao(); 
+TIPO_MATERIAL.forEach((material) => {
+  option = new Option(material, material.toLowerCase());
+  materialEscolar.options[materialEscolar.options.length] = option;
 });
 
-$("#gastarMaterial").focusout(function focusOutz(){
-  habilitarBotao();
-})
+function botaoDesabilitar() {
+  if ($("#nomeAluno").valid() && $("#gastarAte").valid()) {
+    $("#botaozao").attr("disabled", false);
+  }
+  else {
+    $("#botaozao").attr("disabled", true);
+  }
+
+}
+
+$("#nomeAluno").focusout(function () {
+  botaoDesabilitar();
+
+});
+
+$("#gastarAte").focusout(function () {
+  botaoDesabilitar();
+});
+
+function validarProdutos() {
+  console.log("validarProdutos");
+
+  return $('#escolhaOpcao').valid() &&
+         $('#quantidadeProduto').valid() &&
+         $('#precoUnitario').valid();
+}
+
+function calcularTotal(Quantidade, Preco) {
+  console.log("calcularTotal");
+
+  return Quantidade * Preco;
+}
+
+function adicionarItem() {
+
+
+  console.log("adicionarItem");
+  var validarItem = validarProdutos();
+  console.log("validarProdutos");
+
+  if (validarItem) {
+    item.Opcao = $("#escolhaOpcao").val();
+    item.Quantidade = parseInt($("#quantidadeProduto").val());
+    item.Preco = parseInt($("#precoUnitario").val());
+    item.Total = calcularTotal(item.Quantidade, item.Preco);
+    adicionarProdutoTabela(item);
+    mudarCorCamaro(item.Total, $("#gastarAte").val());
+    item = {};
+    $(":input").val("");
+  }
+}
+
+function adicionarProdutoTabela(objeto) {
+  console.log("adicionarProdutoTabela");
+  var tabela = document.querySelector("table");
+  var tr = tabela.insertRow();
+  tr.innerHTML = "<td>" + objeto.Opcao + "</td>" +
+    "<td>" + objeto.Quantidade + "</td>" +
+    "<td>" + objeto.Preco + "</td>" +
+    "<td>" + objeto.Total + "</td>";
+};
+
+function mudarCorCamaro(valorTotal, gastarMaximo){
+  if(gastarMaximo < valorTotal){
+    $("#camaro").removeClass('small-box bg-gradient-green');
+    $("#camaro").addClass('small-box bg-red');
+  }
+  else{
+    $("#camaro").addClass('small-box bg-gradient-green');
+  }
+
+}
+
+function mudarMoney(){
+  h3.innerHTML = "<h3> R$" + ($("#totalDinheiro").val()) + "</h3>";
+}
