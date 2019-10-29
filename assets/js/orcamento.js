c1c05848479e5bc6d934b03aef7488a6086bcb19
gustavogavasussi <gustavogavasussi14@outlook.com>
2019-10-28 21:46:00 -0300

    28/10

--- assets/js/orcamento.js
+++ assets/js/orcamento.js
@@ -6,3 +6,22 @@ arrayMateriais.forEach((material) => {
     option = new Option(material, material.toLowerCase());
     materialEscolar.options[materialEscolar.options.length] = option;
   });
+
+
+function habilitarBotao() {
+if ($('#nomeAluno').valid())
+   ($('#gastarMaterial').valid());{
+  document.getElementById("Button").disabled = false;
+}
+}
+else{
+  document.getElementById("Button").disabled=true;
+}
+
+$("#nomeAluno").focusout(function focusOutz(){
+ habilitarBotao(); 
+});
+
+$("#gastarMaterial").focusout(function focusOutz(){
+  habilitarBotao();
+})
